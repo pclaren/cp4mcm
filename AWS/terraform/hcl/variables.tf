f1eeabfc02e228f8a6e9859ead0d14fa9ed8dfc4
Peter Clarén <peter.claren@gmail.com>
2020-12-04 00:09:03 +0100

    Removed use of ssh_key_label

--- AWS/terraform/hcl/variables.tf
+++ AWS/terraform/hcl/variables.tf
@@ -3,8 +3,4 @@
 ##      Created 07/05/2020 by admin. for test-cam-project
 ##
 #####################################################################
-// variable "ssh_key_label" {}
 variable "hostname" {}
-//variable "domainname" {}
-// variable "cores" {}
-// variable "memory" {}
