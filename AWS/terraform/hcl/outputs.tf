978ce470436bb594847c1724ef5d157e719ccac5
Peter Clarén <peter.claren@gmail.com>
2020-12-03 13:15:07 +0100

    Initial commit

--- AWS/terraform/hcl/outputs.tf
+++ AWS/terraform/hcl/outputs.tf
@@ -5,10 +5,10 @@
 #####################################################################
 
 # Output the public ipv4 address upon successful creation of the VM
- output "instance_ip_addr" {
-   value                 = "${ibm_compute_vm_instance.vm1.ipv4_address}"
-   description           = "The public IP address of the main server instance."
- }
+output "public_ip" {
+  value = aws_instance.vm1.public_ip
+  description           = "The public IP address of the main server instance."
+}
 
 output "private_key" {
    value                 = "${tls_private_key.keyPairForAnsibleUser.private_key_pem}"
