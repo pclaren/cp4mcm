26ab22f2c4fae0f1693a1dcb7d9417bf114a9ee6
Peter Clarén <peter.claren@gmail.com>
2020-12-03 20:57:04 +0100

    Made vpc_security_group_ids variable 0.11.x compatible

--- AWS/terraform/hcl/main.tf
+++ AWS/terraform/hcl/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "centos" {
   ami = "ami-0b850cf02cc00fdc8"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.generated_key.key_name}"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   tags = {
     Name = "${var.hostname}"
