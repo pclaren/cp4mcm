30a0d4e14ca85f2338c1dad8dbf4649deb3d9523
Peter Clarén <peter.claren@gmail.com>
2020-12-03 23:18:28 +0100

    Use locals variables for outputs

--- IBM_Cloud/terraform/hcl/main.tf
+++ IBM_Cloud/terraform/hcl/main.tf
@@ -1,3 +1,8 @@
+locals {
+  private_key = "${tls_private_key.keyPairForAnsibleUser.private_key_pem}"
+  public_ip   = "${ibm_compute_vm_instance.vm1.ipv4_address}"
+}
+
 # Key pair for Ansible user
 resource "tls_private_key" "keyPairForAnsibleUser" {
  algorithm = "RSA"
@@ -22,7 +27,7 @@ resource "ibm_compute_vm_instance" "vm1" {
   datacenter             = "fra02"
   ssh_key_ids            = ["${ibm_compute_ssh_key.ansible_ssh_key.id}", "${data.ibm_compute_ssh_key.public_key.id}"]
   os_reference_code      = "CENTOS_7_64"
-  network_speed          = 100
+  network_speed          = 10
   hourly_billing         = true
   private_network_only   = false
   disks                  = [25]
